daae60a210c4658e72502e393d31829aa507dc94
wuyong1992 <627522616@qq.com>
2017-07-26 00:23:28 +0800

    文件表单

--- src/main/java/com/wuyong/controller/BlogController.java
+++ src/main/java/com/wuyong/controller/BlogController.java
@@ -2,7 +2,9 @@ package com.wuyong.controller;
 
 import com.google.common.collect.Maps;
 import com.wuyong.common.ServerResponse;
+import com.wuyong.pojo.Blog;
 import com.wuyong.service.IBlogService;
+import com.wuyong.service.IFileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +30,8 @@ public class BlogController {
 
     @Autowired
     private IBlogService iBlogService;
+    @Autowired
+    private IFileService iFileService;
 
 
     @RequestMapping(value = "editorImgUpload")
@@ -35,20 +39,20 @@ public class BlogController {
                                HttpServletRequest request, HttpSession session) {
         logger.info("富文本请求收到");
         logger.info("收到内容是：" + files);
-        logger.info("数量："+files.length);
+        logger.info("数量：" + files.length);
 
         Map resultMap = Maps.newHashMap();
 
         String path = request.getSession().getServletContext().getRealPath("/upload");
         if (files != null && files.length > 0) {
             for (MultipartFile file : files) {
-                String targetFileName =iBlogService.richTextImgUpload(file, path);
+                String targetFileName = iBlogService.richTextImgUpload(file, path);
                 logger.info(targetFileName);
                 String url = "http://img.huyunyun.com/" + targetFileName;
                 resultMap.put("link", url);
                 return resultMap;
             }
-        }else {
+        } else {
             //TODO
             return null;
         }
@@ -58,18 +62,38 @@ public class BlogController {
 
     /**
      * blog简介图片，其实就是上面的复用
-     * @param files 文件
-     * @param request   请求
-     * @param session   会话
+     *
+     * @param files   文件
+     * @param request 请求
+     * @param session 会话
      */
     @PostMapping(value = "imgUpload")
     public void imgUpload(@RequestParam(value = "file") MultipartFile[] files,
-                                    HttpServletRequest request, HttpSession session){
+                          HttpServletRequest request, HttpSession session) {
 
         //TODO 保存到对应的blog
 
         this.editorImgUpload(files, request, session);
 
     }
+
+    @PostMapping(value = "blogSubmit")
+    public ServerResponse blogSubmit(@RequestParam(value = "filer") MultipartFile file,
+                                     Blog blog,HttpServletRequest request) {
+
+        logger.info("file====>" + file);
+        logger.info("blog====>" + blog);
+
+        String path = request.getSession().getServletContext().getRealPath("/upload");
+        if (file !=null) {
+            String targetFileName = iFileService.upload(file, path);
+            logger.info(targetFileName);
+            String url = "http://img.huyunyun.com/" + targetFileName;
+            logger.info(url);
+        }
+
+        return ServerResponse.createBySuccess();
+    }
+
 
 }
